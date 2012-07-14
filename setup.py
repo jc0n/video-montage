db229e2fb0f2fac1597963e80997d46d35eda44d
John O'Connor <tehjcon@gmail.com>
2012-07-14 19:23:25 -0400

    Add python setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='video-montage',
+      version='0.1',
+      description='A tool for generating a montage of frames'
+                  'from a video file.',
+      author='John O\'Connor',
+      url='https://github.com/jc0n/video-montage',
+      scripts=['video-montage'])
+
