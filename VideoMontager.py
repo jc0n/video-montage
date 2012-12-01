788c177f38af7f06ca8ad4944d431c3f3dcba95c
John O'Connor <tehjcon@gmail.com>
2012-11-30 20:39:20 -0800

    Bump version

--- VideoMontager.py
+++ VideoMontager.py
@@ -20,7 +20,7 @@ from progressbar import ProgressBar, SimpleProgress, Bar
 from which import which, CommandNotFoundException
 
 __author__ = 'John O\'Connor'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __all__ = ('VideoMontager', )
 
